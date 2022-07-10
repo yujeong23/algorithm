cbb977ba472679b1da3f4a0df54ebc408002dd06
김유정 <yujng789@naver.com>
2022-07-11 00:22:23 +0900

    b_14501

--- 백준/silver/14501_퇴사.py
+++ 백준/silver/14501_퇴사.py
@@ -1,17 +1,11 @@
 N = int(input())
-time = [0] * (N+1)
-pay = [0] * (N+1)
 result = [0] * (N+2)
 ans = 0
 for i in range(N):
     T, P = map(int, input().split())
-    time[i+1] = T
-    pay[i+1] = P
+    ans = max(ans, result[i+1])
+    result[i+1] = ans
+    if i+1 + T in range(N+2):
+        result[i+1 + T] = max(result[i+1 + T], result[i+1]+P)
 
-for j in range(1, N+1):
-
-    if j + time[j] in range(N+2):
-        result[j + time[j]] = max(result[j + time[j]], result[j]+pay[j])
-        ans = max(ans, result[j + time[j]])
-print(result)
 print(max(result))
