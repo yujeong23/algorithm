6f23145010caafe61d089b05b78c7bb14d4467f4
김유정 <yujng789@naver.com>
2022-08-19 01:08:35 +0900

    b_2281

--- 백준/gold/2281_데스노트.py
+++ 백준/gold/2281_데스노트.py
@@ -1,7 +1,25 @@
-def cal(now):
-    if now == n-1:
-        return
-
+# 모든 줄에서 해당 단어를 처음 시작했을 때 나머지 칸 개수로 dp
 n, m = map(int, input().split())
-names = list(int(input()) for _ in range(n))
-cal(0)
+notes = [int(input()) for _ in range(n)]
+dp = [float('inf')] * n
+dp[-1] = 0
+for i in range(n-2, -1, -1):
+    dp[i] = (m - notes[i]) ** 2 + dp[i+1]
+
+    name = notes[i]
+    for j in range(i+1, n):
+        name += notes[j] + 1
+        if name > m:
+            break
+
+        if j == n-1:
+            dp[i] = 0
+            continue
+
+        num = (m - name) ** 2
+        if j+1 in range(n):
+            num += dp[j+1]
+
+        dp[i] = min(dp[i], num)
+
+print(dp[0])
